25f441ee4b68d60a3a568bebd0f16a5685c6ce4b
Froggles <elmsterm@gmail.com>
2026-03-19 13:02:30 -0400

    check logged in user for access to edit and delete character

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -5,9 +5,10 @@ import { deleteCharacter, getCharacterDetails } from "../api/characters";
 import { useAuth } from "../auth/AuthContext";
 
 export default function CharacterDetails() {
-  const { token } = useAuth();
+  const { getProfile } = useAuth();
   const { id } = useParams();
   const [character, setCharacter] = useState();
+  const [profile, setProfile] = useState();
 
   useEffect(() => {
     const tryGetCharacter = async () => {
@@ -15,6 +16,14 @@ export default function CharacterDetails() {
       setCharacter(retrievedCharacter);
     };
     tryGetCharacter();
+  }, []);
+
+  useEffect(() => {
+    const tryGetProfile = async () => {
+      const retrievedProfile = await getProfile();
+      setProfile(retrievedProfile);
+    };
+    tryGetProfile();
   }, []);
 
   if (!character) return <p>Loading character details...</p>;
@@ -38,8 +47,10 @@ export default function CharacterDetails() {
         Owner:
         <Link to={"/users/" + character.user_id}>{character.username}</Link>
       </p>
-      {token && <Link to="edit">Edit</Link>}
-      {token && <button onClick={deleteChar}>Delete</button>}
+      {profile?.id === character.user_id && <Link to="edit">Edit</Link>}
+      {profile?.id === character.user_id && (
+        <button onClick={deleteChar}>Delete</button>
+      )}
       <section className="char-stats">
         <h2>Character Stats</h2>
         <p>{character.hp} HP</p>
