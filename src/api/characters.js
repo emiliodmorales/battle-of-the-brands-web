89894e46aace81d6887e1cb912209c2084347b96
Froggles <elmsterm@gmail.com>
2026-03-19 11:03:37 -0400

    add create character post request

--- src/api/characters.js
+++ src/api/characters.js
@@ -11,3 +11,23 @@ export async function getCharacterDetails(id) {
   const result = await response.json();
   return result;
 }
+
+export async function createCharacter(charData, token) {
+  if (!token) {
+    throw Error("You must be signed in to create a character.");
+  }
+
+  const response = await fetch(API + "/characters", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(charData),
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
