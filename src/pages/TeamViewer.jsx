d886ced1f8793d6f1acf703c00b4a6d47b124890
mwan0906 <mwan0906@gmail.com>
2026-03-23 13:57:49 -0400

    Basic team details

--- src/pages/TeamViewer.jsx
+++ src/pages/TeamViewer.jsx
@@ -1,12 +1,53 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router";
 import { useAuth } from "../auth/AuthContext";
+
+import { getTeamById } from "../api/teams";
+import "../styles/teams.css";
 
 export default function TeamViewer() {
   const { token, getProfile } = useAuth();
   const [team, setTeam] = useState();
 
-  useEffect(() => {}, []);
+  const { id } = useParams();
 
-  return <></>;
+  useEffect(() => {
+    const getTeamDetails = async () => {
+      const t = await getTeamById(id);
+      setTeam(t);
+    };
+    getTeamDetails();
+  }, []);
+
+  if (!team) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <section className="team-menu">
+        <p>Team info here</p>
+        {token && <p>Battle button here</p>}
+        <p>More team stats here</p>
+      </section>
+      <section className="team-view">
+        {team.characters.map((c) => (
+          <section key={c.id} className="character">
+            <img src={c.image} alt={c.name}></img>
+            <Link to={`/characters/${c.id}`}>
+              <h2>{c.name}</h2>
+            </Link>
+            <section className="char-stats">
+              <p>{c.hp} HP</p>
+              <p>{c.attack} ATK</p>
+              <p>{c.defense} DEF</p>
+            </section>
+            <p>
+              {c.ability_name ? `Ability: ${c.ability_name}` : "No Ability"}
+            </p>
+          </section>
+        ))}
+      </section>
+    </>
+  );
 }
