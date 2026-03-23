d7dda4261cf25252e9f8421e0930bd9c57e6e944
mwan0906 <mwan0906@gmail.com>
2026-03-23 10:34:08 -0400

    Mistake with API call

--- src/api/teams.js
+++ src/api/teams.js
@@ -1,4 +1,5 @@
 const API = import.meta.env.VITE_API + "/teams";
+const USER_API = import.meta.env.VITE_API + "/users";
 
 export async function getTeams() {
   const res = await fetch(API);
@@ -12,8 +13,14 @@ export async function getTeamById(id) {
   return team;
 }
 
-export async function getFavoriteTeams() {
-  const res = await fetch(API + "/favorite_teams");
+export async function getFavoriteTeams(token) {
+  const res = await fetch(USER_API + "/favorite_teams", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  });
   const teams = await res.json();
   return teams;
 }
