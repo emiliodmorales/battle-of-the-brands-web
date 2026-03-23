84868f83a7d8a520f1c28f1e2749233281cef838
mwan0906 <mwan0906@gmail.com>
2026-03-23 14:54:00 -0400

    Team details

--- src/pages/TeamViewer.jsx
+++ src/pages/TeamViewer.jsx
@@ -2,20 +2,28 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
 import { useAuth } from "../auth/AuthContext";
 
-import { getTeamById } from "../api/teams";
+import { getTeamById, getTeamHistory } from "../api/teams";
 import "../styles/teams.css";
 
 export default function TeamViewer() {
   const { token, getProfile } = useAuth();
   const [team, setTeam] = useState();
+  const [profile, setProfile] = useState();
 
   const { id } = useParams();
 
   useEffect(() => {
     const getTeamDetails = async () => {
       const t = await getTeamById(id);
-      setTeam(t);
+      const h = await getTeamHistory(id);
+      setTeam({ ...t, history: h });
     };
+    const tryGetProfile = async () => {
+      const retrievedProfile = await getProfile();
+      setProfile(retrievedProfile);
+    };
+
+    tryGetProfile();
     getTeamDetails();
   }, []);
 
@@ -26,25 +34,41 @@ export default function TeamViewer() {
   return (
     <>
       <section className="team-menu">
-        <p>Team info here</p>
-        {token && <p>Battle button here</p>}
-        <p>More team stats here</p>
+        <section>
+          <p>{team.name}</p>
+          <p>Created by {team.username}</p>
+        </section>
+        {token && (
+          <section className="battle-button">
+            {/* TODO - Edit battle link to pass in ID of this team in whatever way is appropriate */}
+            <Link to="/battle">FIGHT</Link>
+          </section>
+        )}
+        {team.history && (
+          <section>
+            <p>Total Battles</p>
+            <p>
+              {Number(team.history.wins)} W /{" "}
+              {Number(team.history.total_battles) - Number(team.history.wins)} L
+            </p>
+          </section>
+        )}
       </section>
       <section className="team-view">
         {team.characters.map((c) => (
           <section key={c.id} className="character">
-            <img src={c.image} alt={c.name}></img>
-            <Link to={`/characters/${c.id}`}>
-              <h2>{c.name}</h2>
-            </Link>
-            <section className="char-stats">
+            <section className="char-info">
+              <Link to={`/characters/${c.id}`}>
+                <h2>{c.name}</h2>
+              </Link>
               <p>{c.hp} HP</p>
               <p>{c.attack} ATK</p>
               <p>{c.defense} DEF</p>
+              <p>
+                {c.ability_name ? `Ability: ${c.ability_name}` : "No Ability"}
+              </p>
             </section>
-            <p>
-              {c.ability_name ? `Ability: ${c.ability_name}` : "No Ability"}
-            </p>
+            <img src={c.image} alt={c.name}></img>
           </section>
         ))}
       </section>
