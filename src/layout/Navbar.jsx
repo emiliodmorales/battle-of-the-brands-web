719d343d86ca67b39428aa40b5a758ce490698d9
Froggles <elmsterm@gmail.com>
2026-03-24 12:14:42 -0400

    set navbar account link to user's profile

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,7 +3,17 @@ import "../styles/navbar.css";
 import { useAuth } from "../auth/AuthContext";
 
 export default function Navbar() {
-  const { token, logout } = useAuth();
+  const { token, logout, getProfile } = useAuth();
+
+  const [profile, setProfile] = useState();
+  useEffect(() => {
+    const tryGetProfile = async () => {
+      const retrievedProfile = await getProfile();
+      setProfile(retrievedProfile);
+    };
+    tryGetProfile();
+  }, []);
+
   return (
     <header id="navbar">
       <NavLink id="brand" to="/">
@@ -16,10 +26,10 @@ export default function Navbar() {
         <NavLink to="/characters">Characters</NavLink>
         {token ? (
           <>
-            <NavLink to="/characters/new">CreateCreature</NavLink>
+            <NavLink to="/characters/new">Create Creature</NavLink>
             <NavLink to="/favorites">Favorites</NavLink>
             <NavLink to="/teams">Teams</NavLink>
-            <NavLink to="/profile">Account</NavLink>
+            <NavLink to={"/users/" + profile.id}>Account</NavLink>
             <button onClick={logout}>Log out</button>
           </>
         ) : (
