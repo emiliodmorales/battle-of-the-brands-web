39411ef4f6d91863a3298a750c02cee7bfe79889
Froggles <elmsterm@gmail.com>
2026-03-24 12:41:02 -0400

    add user endpoints

---
+++ src/api/users.js
@@ -0,0 +1,37 @@
+const USER_API = import.meta.env.VITE_API + "/users";
+
+export async function getUser(id) {
+  const response = await fetch(USER_API + "/" + id);
+  const result = await response.json();
+  return result;
+}
+
+export async function getUserHistory(id) {
+  const response = await fetch(USER_API + "/" + id + "/history");
+  const result = await response.json();
+  return result;
+}
+
+export async function getUserTeams(id) {
+  const response = await fetch(USER_API + "/" + id + "/teams");
+  const result = await response.json();
+  return result;
+}
+
+export async function getUserCharacters(id) {
+  const response = await fetch(USER_API + "/" + id + "/characters");
+  const result = await response.json();
+  return result;
+}
+
+export async function getUserFollowers(id) {
+  const response = await fetch(USER_API + "/" + id + "/followers");
+  const result = await response.json();
+  return result;
+}
+
+export async function getUserFollowing(id) {
+  const response = await fetch(USER_API + "/" + id + "/following");
+  const result = await response.json();
+  return result;
+}
