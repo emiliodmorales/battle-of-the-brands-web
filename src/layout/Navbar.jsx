7fd71b2797a83a1c852390431914f9e25347cc32
Froggles <elmsterm@gmail.com>
2026-03-25 10:37:16 -0400

    remove favorites nav link

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -33,7 +33,6 @@ export default function Navbar() {
         {token ? (
           <>
             <NavLink to="/characters/new">Create Creature</NavLink>
-            <NavLink to="/favorites">Favorites</NavLink>
             <NavLink to="/teams">Teams</NavLink>
             <NavLink to={"/users/" + profile.id}>Account</NavLink>
             <NavLink to="/team-builder">Team Builder</NavLink>
