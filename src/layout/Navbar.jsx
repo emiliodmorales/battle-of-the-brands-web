7fd38a08c3a1054f460e5caa90161ce8c74695e4
Froggles <elmsterm@gmail.com>
2026-03-26 11:24:31 -0400

    navigate home on logout

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,9 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { useAuth } from "../auth/AuthContext";
-import { useState, useEffect } from "react";
 
 export default function Navbar() {
   const { token, logout, profile } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <header
@@ -25,7 +25,14 @@ export default function Navbar() {
             <NavLink to="/teams">Teams</NavLink>
             <NavLink to="/team-builder">Team Builder</NavLink>
             {profile && <NavLink to={"/users/" + profile.id}>Account</NavLink>}
-            <button onClick={logout}>Log out</button>
+            <button
+              onClick={() => {
+                navigate("/");
+                logout();
+              }}
+            >
+              Log out
+            </button>
           </>
         ) : (
           <>
