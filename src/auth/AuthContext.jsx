36cee16b5dec014e4c16363d4e686c6cffde6894
Froggles <elmsterm@gmail.com>
2026-03-27 11:11:03 -0400

    fix get profile guard clause

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -14,7 +14,7 @@ export function AuthProvider({ children }) {
   }, [token]);
 
   useEffect(() => {
-    if (!token) setProfile(null);
+    if (!token) return setProfile(null);
     const tryGetProfile = async () => {
       const response = await fetch(API + "/users/profile", {
         method: "GET",
