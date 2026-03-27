33ee99140c236f5fce8283000d2e97c06ee3acce
Froggles <elmsterm@gmail.com>
2026-03-27 15:08:47 -0400

    add turn message function

--- src/pages/Battle/BattleManager/BattleManager.js
+++ src/pages/Battle/BattleManager/BattleManager.js
@@ -18,6 +18,24 @@ export default class BattleManager {
     return result;
   }
 
+  turnMsg() {
+    if (!this.isBattling) return null;
+    const attacker = this.teamA.getFighter();
+    const defender = this.teamB.getFighter();
+
+    const messages = [];
+
+    messages.push(`${attacker.name} attacked for ${attacker.attack} damage\n`);
+    messages.push(`${defender.name} blocked ${defender.defense} damage\n`);
+
+    messages.push(
+      `${defender.name} counterattacked for ${defender.attack} damage\n`,
+    );
+    messages.push(`${attacker.name} blocked ${attacker.defense} damage\n`);
+
+    return messages;
+  }
+
   nextTurn() {
     if (!this.isBattling) return null;
     const attacker = this.teamA.getFighter();
